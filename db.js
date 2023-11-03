57f10cb3251fe72ce3c9b2ebe2bb64ec4f3bdc02
Alexander Rogan <rogan73@ukr.net>
2023-11-03 16:43:27 +0100

    v.1.3

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config(); // Загрузка переменных окружения из файла .env
+
 const mysql = require("mysql2/promise"); // Обратите внимание на /promise
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "libro",
-  password: ""
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD
 });
 
 module.exports = { pool }; // Экспортируйте пул соединений вместо отдельного соединения
